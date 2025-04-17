ad1bba4aacf6dc6b1576a96a5e221867ef9c6040
adityaazizi <adityaazizi@assistxenterprise.ai>
2025-04-17 13:58:33 +0700

    upload template

--- src/streamlit.py
+++ src/streamlit.py
@@ -6,9 +6,12 @@ import base64                       # Untuk decode/encode data gambar
 from PIL import Image               # Untuk manipulasi gambar
 import io                           # Untuk operasi input/output binary
 import os                           # Untuk akses variabel lingkungan
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Definisi URL API endpoint untuk deteksi objek
-API_URL = os.getenv(URL, "http://localhost:8080/predict")
+API_URL = os.getenv("URL", "http://localhost:8080/predict")
 
 # Konfigurasi halaman Streamlit
 st.set_page_config(page_title="YOLO11 Object Detection", layout="wide")
